47beb7a56fc57d33025101c1fe46be8935bf9504
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-20 14:12:37 +0200

    add mileston 0 and img folder

---
+++ assets/js/main.js
@@ -0,0 +1,38 @@
+/*Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team. 
+Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.*/
+
+//MILESTONE 0: Creare l’array di oggetti con le informazioni fornite.
+const group = [
+    {
+      name: 'Wayne Barnett',
+      role: 'Founder & CEO',
+      Image: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+      name: 'Angela',
+      role: 'Chief Editor',
+      image: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+      name: 'Walter Gordon',
+      role: 'Chief Editor',
+      image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Social Media Manager',
+        image: 'walter-gordon-office-manager.jpg',
+    },
+    {
+      name: 'Scott Estrada',
+      role: 'Developer',
+      image: 'scott-estrada-developer.jpg',
+    },
+    {
+      name: 'Barbara Ramos',
+      role: 'Graphic Designer',
+      image: 'barbara-ramos-graphic-designer.jpg',
+    },
+  ];
+
+  console.log(group)
